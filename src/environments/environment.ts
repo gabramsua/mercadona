5390b2b44b98025e836bc002d8fc661710426445
Gabri <gabramsua@gmail.com>
2022-09-17 12:44:58 +0200

    Fire, Material, PrimeNg

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'mercadona-802bb',
+    appId: '1:1082855388817:web:568405d0baa147b648f130',
+    storageBucket: 'mercadona-802bb.appspot.com',
+    locationId: 'europe-west6',
+    apiKey: 'AIzaSyDveeaPPxMeCv_g-kgpc9Yrw2K29xjXA3s',
+    authDomain: 'mercadona-802bb.firebaseapp.com',
+    messagingSenderId: '1082855388817',
+  },
   production: false
 };
 
